345ccbb57aef0377e9e873f4ee533e143d48bdaa
mdmahmoud32 <23215@esp.mr>
2025-06-05 23:55:27 +0000

    Deploy to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,29 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-    base: '/react-portfolio-template/',
-    plugins: [react()],
-    build: {
-        rollupOptions: {
-            output: {
-                manualChunks(id) {
-                    if (id.includes('node_modules')) {
-                        // Split the swiper plugin library into a separate chunk to avoid a large chunk size on index.js
-                        if (id.includes('swiper'))
-                            return 'swiper';
-                        return;
-                    }
-                }
-            }
+export default defineConfig(({ mode }) => ({
+  base: mode === 'production' ? '/' : '/medmahmoud32/', // 👈 Key change
+  plugins: [react()],
+  build: {
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            if (id.includes('swiper')) return 'swiper'
+            return
+          }
         }
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
+      },
     },
-    css: {
-        preprocessorOptions: {
-            scss: {
-                silenceDeprecations: ["mixed-decls", "color-functions", "global-builtin", "import"],
-            },
-        },
-    },
-})
+  },
+}))
