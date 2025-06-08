e5cd69564645d249297d3ce2ecbfb21750e87dfe
mdmahmoud32 <23215@esp.mr>
2025-06-08 12:53:21 +0000

    updates

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig(({ mode }) => ({
-  base: mode === 'production' ? '/medmahmoud32/' : '/', // 👈 Correct logic
+  base: mode === 'production' ? '/' : '/medmahmoud32/', // 👈 Key change
   plugins: [react()],
   build: {
     rollupOptions: {
